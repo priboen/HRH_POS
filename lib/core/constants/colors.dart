8de0301af55c0f08e1a364a3b6ec284f6cc724d8
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-12-22 21:44:32 +0700

    flutter core

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xff3949AB);
+
+  static const Color grey = Color(0xffB7B7B7);
+
+  static const Color light = Color(0xffF8F5FF);
+
+  static const Color blueLight = Color(0xffC7D0EB);
+
+  static const Color black = Color(0xff000000);
+
+  static const Color white = Color(0xffFFFFFF);
+
+  static const Color green = Color(0xff50C474);
+
+  static const Color red = Color(0xffF4261A);
+
+  static const Color card = Color(0xffE5E5E5);
+
+  static const Color disabled = Color(0xffC8D1E1);
+
+  static const Color subtitle = Color(0xff7890CD);
+
+  static const Color stroke = Color(0xffEFF0F6);
+}
---
+++ lib/core/constants/variables.dart
@@ -0,0 +1,5 @@
+class Variables {
+  static const String appName = 'HRH Point of Sale';
+  static const String apiVersion = 'v1';
+  static const String baseUrl = 'API URL';
+}
