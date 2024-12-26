63d3733926e71a17df00969cdb2ce9ad58c8e4ca
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-12-26 09:23:01 +0700

    add function for dialog success and error

--- lib/core/extensions/build_context_ext.dart
+++ lib/core/extensions/build_context_ext.dart
@@ -1,9 +1,95 @@
 import 'package:flutter/material.dart';
 
+import '../assets/assets.dart';
+import '../components/components.dart';
+import '../constants/colors.dart';
+
 extension BuildContextExt on BuildContext {
   double get deviceHeight => MediaQuery.of(this).size.height;
 
   double get deviceWidth => MediaQuery.of(this).size.width;
+}
+
+extension AlertDialogExt on BuildContext {
+  void showDialogSuccess(String title, String subtitle) {
+    showDialog(
+      context: this,
+      builder: (context) {
+        return AlertDialog(
+          content: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Assets.icons.alert.success.svg(
+                  width: 150.0,
+                  height: 150.0,
+                  fit: BoxFit.cover,
+                ),
+                const SpaceHeight(32.0),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SpaceHeight(16.0),
+                Text(
+                  subtitle,
+                  style: const TextStyle(
+                    color: AppColors.green,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void showDialogError(String title, String subtitle) {
+    showDialog(
+      context: this,
+      builder: (context) {
+        return AlertDialog(
+          content: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Assets.icons.alert.error.image(
+                  width: 150.0,
+                  height: 150.0,
+                  fit: BoxFit.cover,
+                ),
+                const SpaceHeight(32.0),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SpaceHeight(16.0),
+                Text(
+                  subtitle,
+                  style: const TextStyle(
+                    color: AppColors.red,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
 
 extension NavigatorExt on BuildContext {
