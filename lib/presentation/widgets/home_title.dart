0f83ed836219b2f27e07cb5a03fe5e04e9c86cdf
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-12-27 16:09:34 +0700

    add Widget

---
+++ lib/presentation/widgets/home_title.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:hrh_pos/core/extensions/extensions.dart';
+
+import '../../../core/components/search_input.dart';
+import '../../../core/constants/colors.dart';
+
+
+
+class HomeTitle extends StatelessWidget {
+  final TextEditingController controller;
+  final Function(String value)? onChanged;
+
+  const HomeTitle({
+    super.key,
+    required this.controller,
+    this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'HRH POS',
+              style: TextStyle(
+                color: AppColors.primary,
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 4.0),
+            Text(
+              DateTime.now().toFormattedDate(),
+              style: const TextStyle(
+                color: AppColors.subtitle,
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          width: 260.0,
+          child: SearchInput(
+            controller: controller,
+            onChanged: onChanged,
+            hintText: 'Search for food, coffe, etc..',
+          ),
+        ),
+      ],
+    );
+  }
+}
