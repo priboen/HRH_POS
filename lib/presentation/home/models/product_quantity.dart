858c50f753902e8420bf96840695a045f645b55c
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-12-28 11:04:40 +0700

    Product Quantity

---
+++ lib/presentation/home/models/product_quantity.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import 'package:hrh_pos/data/models/response/product_response_model.dart';
+
+class ProductQuantity {
+  final Product product;
+  int quantity;
+
+  ProductQuantity({required this.product, required this.quantity});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'product': product.toMap(),
+      'quantity': quantity,
+    };
+  }
+
+  Map<String, dynamic> toLocalMap(int orderId) {
+    return {
+      'id_order': orderId,
+      'id_product': product.idProduct,
+      'quantity': quantity,
+      'price': product.price
+    };
+  }
+
+  factory ProductQuantity.fromMap(Map<String, dynamic> map) {
+    return ProductQuantity(
+      product: Product.fromMap(map['product']),
+      quantity: map['quantity']?.toInt() ?? 0,
+    );
+  }
+  factory ProductQuantity.fromLocalMap(Map<String, dynamic> map) {
+    return ProductQuantity(
+      product: Product.fromMap(map),
+      quantity: map['quantity']?.toInt() ?? 0,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ProductQuantity.fromJson(String source) =>
+      ProductQuantity.fromMap(json.decode(source));
+}
