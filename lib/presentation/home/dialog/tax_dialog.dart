8645d8be2e4eba9971f818732640d53aa0f1738e
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-12-29 22:36:31 +0700

    create tax dialog

---
+++ lib/presentation/home/dialog/tax_dialog.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hrh_pos/core/core.dart';
+import 'package:hrh_pos/core/extensions/extensions.dart';
+import 'package:hrh_pos/presentation/home/bloc/checkout/checkout_bloc.dart';
+import 'package:hrh_pos/presentation/home/bloc/get_tax/get_tax_bloc.dart';
+
+class TaxDialog extends StatefulWidget {
+  const TaxDialog({super.key});
+
+  @override
+  State<TaxDialog> createState() => _TaxDialogState();
+}
+
+class _TaxDialogState extends State<TaxDialog> {
+    @override
+  void initState() {
+    context.read<GetTaxBloc>().add(const GetTaxEvent.getTax());
+    super.initState();
+  }
+
+  int taxIdSelected = 0;
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Stack(
+        alignment: Alignment.center,
+        children: [
+          const Text(
+            'DISKON',
+            style: TextStyle(
+              color: AppColors.primary,
+              fontSize: 28,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: IconButton(
+              onPressed: () {
+                context.pop();
+              },
+              icon: const Icon(
+                Icons.cancel,
+                color: AppColors.primary,
+                size: 30.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+      content: BlocBuilder<GetTaxBloc, GetTaxState>(
+        builder: (context, state) {
+          return state.maybeWhen(
+            orElse: () => const SizedBox.shrink(),
+            loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+            loaded: (taxs) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: taxs
+                    .map(
+                      (tax) => ListTile(
+                        title: Text('Nama Pajak: ${tax.namaPajak}'),
+                        subtitle: Text('Tarif Pajak (${tax.pajakPersen}%)'),
+                        contentPadding: EdgeInsets.zero,
+                        textColor: AppColors.primary,
+                        trailing: Checkbox(
+                          value: tax.idPajak == taxIdSelected,
+                          onChanged: (value) {
+                            setState(() {
+                              taxIdSelected = tax.idPajak!;
+                              context.read<CheckoutBloc>().add(
+                                    CheckoutEvent.addTax(
+                                      tax,
+                                    ),
+                                  );
+                            });
+                          },
+                        ),
+                        onTap: () {
+                          // context.pop();
+                        },
+                      ),
+                    )
+                    .toList(),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
