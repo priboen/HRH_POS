2aada60f4389a9a8006ed8c07e97a58ad6004965
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-01-09 17:31:28 +0700

    line block submitOrder

--- lib/presentation/home/controllers/confirm_payment_controller.dart
+++ lib/presentation/home/controllers/confirm_payment_controller.dart
@@ -21,4 +21,46 @@ class ConfirmPaymentController {
   void setExactTotal() {
     totalPriceController.text = subtotal.currencyFormatRp;
   }
+
+  // Future<void> submitOrder({
+  //   required String selectedOption,
+  //   required int? selectedPaymentId,
+  //   required int? selectedDiscountId,
+  //   required int? selectedTaxId,
+  //   required List<OrderItem> orderItems,
+  //   required BuildContext context,
+  // }) async {
+  //   final authData = await AuthLocalDatasources().getAuthData();
+  //   final idUser = authData.data?.id;
+
+  //   final paymentAmount = int.tryParse(
+  //         totalPriceController.text.replaceAll(RegExp(r'[^0-9]'), ''),
+  //       ) ??
+  //       0;
+
+  //   final returnPayment = paymentAmount - subtotal;
+
+  //   final totalItem = orderItems.fold<int>(
+  //     0,
+  //     (previousValue, item) => previousValue + (item.quantity ?? 0),
+  //   );
+
+  //   final orderRequest = OrderRequestModel(
+  //     idUser: idUser,
+  //     layanan: selectedOption,
+  //     idPayment: selectedPaymentId,
+  //     idPajak: selectedTaxId,
+  //     idDiskon: selectedDiscountId,
+  //     subTotal: subtotal,
+  //     totalItem: totalItem,
+  //     total: subtotal,
+  //     paymentAmount: paymentAmount,
+  //     returnPayment: returnPayment,
+  //     transaksiTime: DateTime.now(),
+  //     orderItems: orderItems,
+  //   );
+
+  //   context.read<AddOrderBloc>().add(AddOrderEvent.addOrder(orderRequest.idDiskon,
+  //       orderRequest.idPajak, orderRequest.paymentAmount, orderRequest.layanan, orderRequest.idPayment, orderRequest.orderItems));
+  // }
 }
