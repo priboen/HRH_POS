6f24c61781bc003b551b973c8c3dbf18f4299028
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-01-09 18:53:06 +0700

    delete add discount

--- lib/presentation/home/bloc/add_discount/add_discount_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'add_discount_bloc.dart';
-
-@freezed
-class AddDiscountState with _$AddDiscountState {
-  const factory AddDiscountState.initial() = _Initial;
-  const factory AddDiscountState.loading() = _Loading;
-  const factory AddDiscountState.success() = _Success;
-  const factory AddDiscountState.error(String message) = _Error;
-}
