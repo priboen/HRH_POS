35d3b788c0afbe03f6f034eacd9600f3cf1f30cf
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-01-09 18:53:49 +0700

    delete discount delete features

--- lib/presentation/home/bloc/delete_discount/delete_discount_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'delete_discount_bloc.dart';
-
-@freezed
-class DeleteDiscountEvent with _$DeleteDiscountEvent {
-  const factory DeleteDiscountEvent.started() = _Started;
-  const factory DeleteDiscountEvent.deleteDiscount({
-    required int idDiskon,
-  }) = _DeleteDiscount;
-}
